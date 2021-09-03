35e5a9f8b86ff0dd432aa0b9a6500e147468c9f4
HanhDT1 <v.hanhdt1@vinid.net>
2021-09-03 11:12:17 +0700

    Done bai 1 homework 3

--- homework3.py
+++ homework3.py
@@ -1,5 +1,20 @@
+
+# Bài 1: Chương trình in ra các hình sau (dùng ký tự '*' và ký tự space) với n là số dòng
+print("\n---- Bài 1: Print Star ----\n")
 try:
-    n = int(input("Nhập số nguyên bạn muốn in ra "))
+    n = abs(int(input("Nhập số nguyên bạn muốn in ra ")))
+    star_str = " *"
+    space_str = "  "
+    for star_number in range(n):
+        space_number  = n -star_number 
+        print((space_number + 1) * space_str + (star_number + 1) * star_str)
+
+    for star_number in range(n):
+        space_number  = n -star_number 
+        print((star_number + 1) * star_str + (space_number + 1) * space_str )
 
 except:
-    print("Bạn cần nhập số nguyên");
+    print("Bạn cần nhập số nguyên")
+
+# Bài 2: Trả ra từ điển với key là các số trong list, value là số lần xuất hiện của số trong list
+print("\n---- Bài 1: Đếm số ----\n")    
